81ade203af94505894aaa7c3ef7f657c806d3913
izhynko <zhinkoilya1993@gmail.com>
2023-03-13 22:44:02 +0100

    fix config

--- src/main/kotlin/com/example/chatgpttelegrambot/config/OpenAIConfig.kt
+++ src/main/kotlin/com/example/chatgpttelegrambot/config/OpenAIConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class TelegramConfig(
+class OpenAIConfig(
     @Value("\${open-ai.api-key}")
     private val apiKey: String,
 ) {
